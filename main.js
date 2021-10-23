611ef6fe0bd807db0f423aa6b2545d7fc994f18b
Bruno Padilha <92052091+BrunosPadilha@users.noreply.github.com>
2021-10-23 06:10:43 -0300

    Version 1.0

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'BrunosPadilha',
+  youtube: 'channel/UCp7aa0ocPo8D0Y5TOguDotw',
+  facebook: '100003383602647',
+  instagram: 'brunospadilha_',
+  twitter: 'Padilha_Brunno'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
